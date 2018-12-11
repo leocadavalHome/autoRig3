64940ed26631804a851acf9988344d6031590a54
Unknown <leo.cadaval@vetorzero.com.br>
2018-12-11 10:16:40 -0200

    facial

--- composites/facial.py
+++ composites/facial.py
@@ -8,16 +8,28 @@ sel = pm.ls(sl=True)
 neutro = sel.pop(-1)
 
 mCorners = mouthCorners.MouthCorners(name='MouthCorners', receiptMesh=neutro, targetShapes=sel)
-L_eyeBrow = eyeBrows.EyeBrow(name='L_brow', mesh='corpo1')
-R_eyeBrow = eyeBrows.EyeBrow(name='R_brow', mesh='corpo1', flipAxis=True)
+L_eyeBrow = eyeBrows.EyeBrow(name='L_brow', mesh=neutro)
+R_eyeBrow = eyeBrows.EyeBrow(name='R_brow', mesh=neutro, flipAxis=True)
 mouthJaw = jaw.Jaw(name='jaw')
+
+mCorners.getGuideFromScene()
+L_eyeBrow.getGuideFromScene()
+mouthJaw.getGuideFromScene()
 
 L_eyeBrow.doGuide()
 R_eyeBrow.mirrorConnectGuide(L_eyeBrow)
+
 mCorners.doGuide()
 mouthJaw.doGuide()
 
 mouthJaw.doRig()
+mCorners.doRig()
+
 L_eyeBrow.doRig()
 R_eyeBrow.doRig()
-mCorners.doRig()
+
+
+print mCorners.bsDict
+
+mouthCorners.splitSidesAPI(targetObj=pm.PyNode('male02_eyeblink_geo'), sourceObj=pm.PyNode('male02_body_receipt'), falloff=0.2)
+mouthCorners.splitSidesAPI(targetObj=pm.PyNode('male02_half_eyeblink_geo'), sourceObj=pm.PyNode('male02_body_receipt'), falloff=0.2)
